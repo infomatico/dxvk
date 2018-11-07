9c1707c6d0a92a330052ead0c8036f2d2b8f9df5
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2018-11-07 11:57:36 +0100

    [dxbc] Use subgroup operations for early discard
    Can improve performance in some complex fragment shaders.

--- src/dxbc/dxbc_options.h
+++ src/dxbc/dxbc_options.h
@@ -11,6 +11,10 @@ namespace dxvk {
     /// Use the ShaderImageReadWithoutFormat capability.
     /// Enabled by default on GPUs which support this.
     bool useStorageImageReadWithoutFormat = false;
+
+    /// Use subgroup operations to discard fragment
+    /// shader invocations if derivatives remain valid.
+    bool useSubgroupOpsForEarlyDiscard = false;
   };
   
 }
