5667d9b24256b0090d602c1277a9971667731997
Philip Rebohle <philip.rebohle@tu-dortmund.de>
2018-11-11 17:49:11 +0100

    [dxbc] Use clustered subgroup operations on supported hardware

--- src/dxbc/dxbc_compiler.cpp
+++ src/dxbc/dxbc_compiler.cpp
@@ -3884,7 +3884,13 @@ namespace dxvk {
         uint32_t killSubgroup = m_module.opGroupNonUniformLogicalAnd(
           m_module.defBoolType(),
           m_module.constu32(spv::ScopeSubgroup),
-          spv::GroupOperationReduce, killState, 0);
+          m_moduleInfo.options.useSubgroupOpsClustered
+            ? spv::GroupOperationClusteredReduce
+            : spv::GroupOperationReduce,
+          killState,
+          m_moduleInfo.options.useSubgroupOpsClustered
+            ? m_module.constu32(4)
+            : 0);
         
         DxbcConditional cond;
         cond.labelIf  = m_module.allocateId();
@@ -6177,6 +6183,9 @@ namespace dxvk {
       if (m_moduleInfo.options.useSubgroupOpsForEarlyDiscard) {
         m_module.enableCapability(spv::CapabilityGroupNonUniform);
         m_module.enableCapability(spv::CapabilityGroupNonUniformArithmetic);
+
+        if (m_moduleInfo.options.useSubgroupOpsClustered)
+          m_module.enableCapability(spv::CapabilityGroupNonUniformClustered);
       }
     }
     
